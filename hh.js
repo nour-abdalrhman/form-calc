fc8283452dc91400fd0edba9fcad9aae6edbc974
Nour Mohammed <nour.tebo300@gmail.com>
2024-11-21 10:42:18 +0200

    Initial commit

---
+++ hh.js
@@ -0,0 +1,7 @@
+function addNumbers() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var result = num1 + num2;
+    
+    document.getElementById('result').innerText = "Result: " + result;
+    }
